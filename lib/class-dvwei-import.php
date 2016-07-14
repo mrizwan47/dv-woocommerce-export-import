5e70e219b7d99787e2ff90054ef42a3d25774b0a
Rizwan <m.rizwan_47@yahoo.com>
2016-07-14 22:33:32 +0500

    initial import class

---
+++ lib/class-dvwei-import.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * DV Woocommerce Import Class
+ *
+ * Class to handle import of woocommerce data
+ *
+ * @author      Rizwan <rizwan@designsvalley.com>
+ * @package     DVWoocommerceExportImport
+ * @copyright   2016 Designsvalley
+ * @link        http://designsvalley.com/
+ * @since       Version 1.0
+ * @TODO				some commenting and make class more extensible
+ */
+
+class DVWEI_Import{
+	// TODO
+}
