44cc1be6f6e3d49c0816e88740f58eb5c5f0b393
Rizwan <m.rizwan_47@yahoo.com>
2016-07-20 18:54:51 +0500

    another work on import

--- lib/class-dvwei-import.php
+++ lib/class-dvwei-import.php
@@ -45,7 +45,8 @@ class DVWEI_Import{
 		}
 
 		$args = wp_parse_args( $args, array(
-			'truncate_before_import'	=> false
+			'truncate_before_import'	=> false,
+			'replace_old_data'				=> false
 		));
 
 		/**
@@ -83,11 +84,41 @@ class DVWEI_Import{
 	 */
 	public function import(){
 
+		$this->import_taxonomies();
+		die;
 		$this->import_basic_products();
+		die;
 
 		// TODO keep adding new sheets
 
 	}
+
+	/**
+	 * Import Categories, Tags and Attributes
+	 */
+	function import_taxonomies(){
+
+		$categories_sheet			= $this->phpexcel_data->getSheetByName('categories');
+		$categories						=	$categories_sheet->toArray(null,true,true,true);
+
+		$cat_keys							=	array_shift($categories);
+
+		foreach( $categories as $cat_vals ){
+
+			$category	=	array_combine($cat_keys, $cat_vals);
+
+			$res	=	wp_insert_term( $category['name'], 'product_cat', array(
+				'description'		=> $category['description'],
+				'slug'					=> $category['slug'],
+				'parent'				=> $category['parent_id']
+			) );
+
+			update_term_meta( $res['term_id'], 'display_type', $category['display_type'] );
+
+		}
+
+	}
+
 
 	/**
 	 * Import Basic Products
@@ -96,12 +127,31 @@ class DVWEI_Import{
 
 		$sheet					= $this->phpexcel_data->getSheetByName('products');
 		$data						=	$sheet->toArray(null,true,true,true);
-
-		//array_flip(current());
+		$first					=	true;
 
 		foreach( $data as $product ){
 
+			if( !$first ){
 
+				$add_p_data = array(
+				  'post_title'    => wp_strip_all_tags( $product['B'] ),
+				  'post_content'  => $product['G'],
+				  'post_status'   => $product['AG'],
+					'post_excerpt'	=> $product['F'],
+					'post_type'			=> 'product',
+					'comment_status'=> $product['AF']
+				);
+
+				if( $this->args['replace_old_data'] )
+					$add_p_data['ID']		= $product['A'];
+
+				print_r($product);
+				//wp_insert_post( $add_p_data );
+
+			}else{
+				$first	=	false;
+				print_r($product);
+			}
 
 		}
 
